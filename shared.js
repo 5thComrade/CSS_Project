3d9dc82dc222be975523b60b07727ca466a3867a
Antony Chiramel <antonychiramel07@gmail.com>
2020-10-11 20:08:25 +0530

    Animations and Transitions module complete

--- shared.js
+++ shared.js
@@ -1,6 +1,19 @@
 let backdrop = document.querySelector(".backdrop");
 let modal = document.querySelector(".modal");
 let mobileNav = document.querySelector(".mobile-nav");
+let ctaButton = document.querySelector(".main-nav__item--cta");
+
+ctaButton.addEventListener("animationstart", function (event) {
+  console.log("Animation Started", event);
+});
+
+ctaButton.addEventListener("animationend", function (event) {
+  console.log("Animation Ended", event);
+});
+
+ctaButton.addEventListener("animationiteration", function (event) {
+  console.log("Animation iteration", event);
+});
 
 let selectPlanButton = document
   .querySelectorAll(".plan button")
@@ -22,14 +35,14 @@ document
 
 function displayModal() {
   backdrop.style.display = "block";
-  modal.style.display = "block";
+  modal.classList.add("open");
 }
 
 function removeModal() {
   backdrop.style.display = "none";
   mobileNav.style.display = "none";
   if (modal) {
-    modal.style.display = "none";
+    modal.classList.remove("open");
   }
 }
 
